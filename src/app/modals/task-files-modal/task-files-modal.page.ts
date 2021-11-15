394e5b892e24e6fc2b1dfe1c1624e943339d21bb
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-11-14 19:09:23 -0600

    image preview in image upload modal

--- src/app/modals/task-files-modal/task-files-modal.page.ts
+++ src/app/modals/task-files-modal/task-files-modal.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { ModalController } from "@ionic/angular";
 import { RestService } from '../../services/rest.service';
+import { DomSanitizer, SafeResourceUrl, SafeHtml} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-task-files-modal',
@@ -10,10 +11,14 @@ import { RestService } from '../../services/rest.service';
 export class TaskFilesModalPage implements OnInit {
   @Input() id_task;
   files = [];
+  selectedFile: File = null;
+  imgPreview = null;
+  @ViewChild('inputfile', {read: ElementRef}) private inputfile: ElementRef;
 
   constructor(
     private modalController: ModalController,
     private restService : RestService,
+    private sanitizer: DomSanitizer,
   ) { }
 
   ngOnInit() {
@@ -30,6 +35,40 @@ export class TaskFilesModalPage implements OnInit {
     this.restService.get_method(`task/${this.id_task}`,'').subscribe(result =>{
       this.files = result.data.files;
     });
+  }
+
+  removeImg(){
+    this.imgPreview = null;
+    this.selectedFile = null;
+    this.inputfile.nativeElement.value = null;
+  }
+
+  cleanURL(oldURL: string): SafeResourceUrl {
+    //return this.sanitizer.bypassSecurityTrustHtml(oldURL);
+    return this.sanitizer.bypassSecurityTrustResourceUrl(oldURL);
+  }
+
+  onFileSelected(event){
+    this.selectedFile = <File>event.target.files[0];
+    let reader = new FileReader();
+    reader.onload = (event:any) => {
+      this.imgPreview = event.target.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+    //let fileList: FileList = event.target.files;  
+    //let file: File = fileList[0];
+  }
+
+  onUpload(){
+    const formData:FormData = new FormData();
+    formData.append('file',this.selectedFile,this.selectedFile.name);
+    formData.append('task',this.id_task);
+    //this.restService.post_method('file',{'file':formData.get('file'),'task':this.id_task}).subscribe(result =>{
+      //this.load_files();
+    //});
+    //this.restService.post_method('file',formData).subscribe(result =>{
+      //this.load_files();
+    //});
   }
 
 }
